dc7172c95a1830ca0a407224f8957390b8e0e1c3
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-04-16 18:08:17 +0300

    Revert "adds changes"
    This reverts commit b5355d773dc8c87aae5a8698b38a18e16af5a075.

--- webpack.config.js
+++ webpack.config.js
@@ -17,11 +17,7 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
+      }
     ],
   },
 };
